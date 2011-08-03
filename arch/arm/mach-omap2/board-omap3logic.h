66d6006b1b03aab0df4ab31e2fb4ef3bb6c97faa
peterb <peterb@d7e73fac-2431-0410-abd5-fd325483733a>
2011-08-03 20:50:30 +0000

    - Changes for DM37LINUX-257:  - Add board-omap3logic.h to allow omap3logic_init to access    omap3logic_hang variable.
    git-svn-id: svn+ssh://cvs.logicpd.com/cvs/eps_svn/software/products/linux/kernels/linux-3.0@19220 d7e73fac-2431-0410-abd5-fd325483733a

---
+++ arch/arm/mach-omap2/board-omap3logic.h
@@ -0,0 +1,13 @@
+/*
+ * linux/arch/arm/mach-omap2/board-omap3logic.h
+ *
+ * Copyright (C) 2011 Logic Product Development
+ *
+ * Initial code: Peter Barada
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+extern volatile int omap3logic_hang;
