4811a48f797d3d36eca29d61dc9cc4cea3b94d87
peterb <peterb@d7e73fac-2431-0410-abd5-fd325483733a>
2011-08-10 19:36:08 +0000

    - Changes for DM37LINUX-265:  - Its CONFIG_MMC_DEBUG_CONTROLLER_MASK, not MMC_DEBUG_CONTROLLER_BITMASK....
    git-svn-id: svn+ssh://cvs.logicpd.com/cvs/eps_svn/software/products/linux/kernels/linux-3.0@19502 d7e73fac-2431-0410-abd5-fd325483733a

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -327,7 +327,7 @@ struct _mmc_csd {
 #define MMC_SWITCH_MODE_CLEAR_BITS	0x02	/* Clear bits which are 1 in value */
 #define MMC_SWITCH_MODE_WRITE_BYTE	0x03	/* Set target to value */
 
-#ifdef MMC_DEBUG_CONTROLLER_BITMASK
+#ifdef CONFIG_MMC_DEBUG_CONTROLLER_BITMASK
 extern int mmc_debug_controller_bitmask;
 #define MMC_DEBUG_CONTROLLER(idx) (mmc_debug_controller_bitmast & (1<<(idx)))
 #else
