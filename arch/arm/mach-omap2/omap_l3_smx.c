735f6ff4aa1bece31c845008af450f041299f49e
peterb <peterb@d7e73fac-2431-0410-abd5-fd325483733a>
2012-03-09 18:38:24 +0000

    - Changes for DM37LINUX-538:  - Add code to dump L3_STATUS_0 register on L3 interconnect timeout.  If    "printk-debug" is on the command line, then on reset x-loader should    be able to show which peripheral timed out.
    git-svn-id: svn+ssh://svn/cvs/eps_svn/software/products/linux/kernels/linux-3.0@27041 d7e73fac-2431-0410-abd5-fd325483733a

--- arch/arm/mach-omap2/omap_l3_smx.c
+++ arch/arm/mach-omap2/omap_l3_smx.c
@@ -184,7 +184,14 @@ static irqreturn_t omap3_l3_app_irq(int irq, void *_l3)
 		 * of such errors and handle the others. timeout error
 		 * is severe and not expected to occur.
 		 */
+#if 1
+		if (status & L3_STATUS_0_TIMEOUT_MASK) {
+			printk("%s: L3_STATUS_0: %16llx\n", __FUNCTION__, status);
+			BUG();
+		}
+#else
 		BUG_ON(status & L3_STATUS_0_TIMEOUT_MASK);
+#endif
 	} else {
 		status = omap3_l3_readll(l3->rt, L3_SI_FLAG_STATUS_1);
 		/* No timeout error for debug sources */
